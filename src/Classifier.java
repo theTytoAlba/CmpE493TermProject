c7ac1c8cdde932cf7fddb8f58059261ba08649e7
Irmak Kavasoglu <irmak.kavasoglu@getir.com>
2018-05-16 13:48:15 +0300

    Added stub for multinomial bayes

---
+++ src/Classifier.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public class Classifier {
+    public static void multinomialBayes(ArrayList<Rotation> rotations) {
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,7 @@ public class Main {
         readInformation();
         ArrayList<Rotation> rotations = createRotations();
         StatisticsHelper.createPositiveNegativeWordStatistics();
+        Classifier.multinomialBayes(rotations);
     }
 
     /**
